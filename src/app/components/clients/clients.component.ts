e1f19feb85bff8eb812c572c543beb13015fe814
Stanko <s.sustek105@gmail.com>
2017-10-25 21:38:58 +0200

    client list OK .. sidebar

--- src/app/components/clients/clients.component.ts
+++ src/app/components/clients/clients.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ClientService } from '../../services/client.service';
 import  { Client } from '../../model/Client';
 
+
 @Component({
   selector: 'app-clients',
   templateUrl: './clients.component.html',
@@ -17,8 +18,17 @@ export class ClientsComponent implements OnInit {
   ngOnInit() {
     this.clientService.getClients().subscribe(clients => {
       this.clients = clients;
+      this.getTotalOwed();
      // console.log(clients);
     })
+  }
+  getTotalOwed() {
+    let total = 0;
+    for (let i = 0; i < this.clients.length; i++) {
+      total = total + parseFloat(this.clients[i].balance.toString());
+    }
+    this.totalOwd = total;
+
   }
 
 }
